03d07120e6dd712220eef534e9d26eaabda7f898
takanorig <takanorig@gmail.com>
2019-04-12 13:14:42 +0900

    karate version up to 0.9.2

--- src/test/java/examples/rentacycles/RentaCyclesRunner.java
+++ src/test/java/examples/rentacycles/RentaCyclesRunner.java
@@ -1,9 +1,10 @@
 package examples.rentacycles;
 
+import com.intuit.karate.KarateOptions;
 import com.intuit.karate.junit4.Karate;
-
 import org.junit.runner.RunWith;
 
+@KarateOptions(features = "classpath:examples/rentacycles", tags = "~@ignore") 
 @RunWith(Karate.class)
 public class RentaCyclesRunner {
 
